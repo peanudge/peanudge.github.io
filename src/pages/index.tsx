d4e0f54fd5307009862ad0165fcc0fd02620cab9
peanut-lover <jihoson94@gmail.com>
2023-03-28 13:29:42 +0900

    Update main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,8 @@
 import React from "react";
-import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import styles from "./index.module.css";
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/blog">
-            기술 블로그
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
@@ -29,7 +11,24 @@ export default function Home(): JSX.Element {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-      <HomepageHeader />
+      <div className={styles.main}>
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      </div>
+      <div className={styles.buttons}>
+        <div className={styles.contentBox}>
+          <p>관심있는 기술에 대한 이야기</p>
+          <Link className={styles.button} to="/blog">
+            블로그
+          </Link>
+        </div>
+        <div className={styles.contentBox}>
+          <p>정리하고 싶은 기술적인 상세한 내용</p>
+          <Link className={styles.button} to="/blog">
+            문서
+          </Link>
+        </div>
+      </div>
     </Layout>
   );
 }
