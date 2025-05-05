50589f61242b758070139af7bb7151bb7e9f0dbd
peanut-lover <jihoson94@gmail.com>
2025-05-05 14:02:29 +0900

    Change basePath to empty string

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/",
+  basePath: "",
 
   /**
    * Disable server-based image optimization. Next.js does not support
